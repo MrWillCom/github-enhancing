c5cc9149a33b0fd2029005cb7a60461f61092ba0
MrWillCom <mr.will.com@outlook.com>
2020-12-13 17:56:51 +0800

    Initialize extension

---
+++ build.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+console.log('Building release...')
+
+const light = fs.readFileSync('./styles/light.css', { encoding: 'utf8' })
+const dark = fs.readFileSync('./styles/dark.css', { encoding: 'utf8' })
+const styles = fs.readFileSync('./styles/styles.css', { encoding: 'utf8' })
+
+const release = `(() => {
+    var body = document.body;
+    var style = document.createElement('style');
+    style.innerHTML = \`[data-color-mode=auto], [data-color-mode=light] { ${light} } @media (prefers-color-scheme: dark) { [data-color-mode=auto] { ${dark} } } [data-color-mode=dark] { ${dark} }\n${styles}\`;
+    body.appendChild(style);
+})()`
+
+console.log('Writing release...')
+fs.writeFileSync('./js/index.js', release, { encoding: 'utf8' })
+console.log('Completed.')
---
+++ init.js
@@ -0,0 +1,5 @@
+require('fs').mkdirSync('./js/');
+
+console.log('./js/ created.')
+
+console.log('Initialized.')
