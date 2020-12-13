ae3194f9f8f155178da9752c7fb255bc6f408a54
MrWillCom <mr.will.com@outlook.com>
2020-12-13 19:05:47 +0800

    Add main.js

--- build.js
+++ build.js
@@ -5,12 +5,15 @@ console.log('Building release...')
 const light = fs.readFileSync('./styles/light.css', { encoding: 'utf8' })
 const dark = fs.readFileSync('./styles/dark.css', { encoding: 'utf8' })
 const styles = fs.readFileSync('./styles/styles.css', { encoding: 'utf8' })
+const scripts = fs.readFileSync('./js/main.js', {encoding: 'utf8'})
 
 const release = `(() => {
     var body = document.body;
     var style = document.createElement('style');
     style.innerHTML = \`[data-color-mode=auto], [data-color-mode=light] { ${light} } @media (prefers-color-scheme: dark) { [data-color-mode=auto] { ${dark} } } [data-color-mode=dark] { ${dark} }\n${styles}\`;
     body.appendChild(style);
+
+    ${scripts}
 })()`
 
 console.log('Writing release...')
---
+++ js/main.js
@@ -0,0 +1,1 @@
+console.log('GitHub Enhancing is launched.')
