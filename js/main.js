9f16f25aab7e326549a42dfb0a4399882c2e533b
MrWillCom <mr.will.com@outlook.com>
2021-06-23 11:24:52 +0000

    Add effects for releases section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 console.log('GitHub Enhancing is launched.')
+
+document.querySelectorAll(".h4.mb-3").forEach((el) => {
+    el.parentNode.parentElement.setAttribute("data-gheh-section-type", el.innerText.split(" ")[0].toLowerCase());
+})
